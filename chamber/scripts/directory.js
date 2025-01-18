1df8f47599db14ea91acf99038699cebfd6ad68a
dmquiroga11 <qui24003@byui.edu>
2025-01-17 19:47:51 -0500

    json work

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -19,3 +19,42 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
     nav.classList.remove("visible");
 });
+
+/*--------JSON WORK--------*/
+async function fetchMembers() {
+    try {
+      const response = await fetch('members.json'); 
+      const companies = await response.json();
+      
+      const membersContainer = document.getElementById('members');
+      companies.forEach(company => {
+        const section = document.createElement('section');
+        section.classList.add('boxbusiness');
+        
+        section.innerHTML = `
+          <div class="busihead">
+            <h3>${company.name}</h3>
+            <p>${company.otherInfo}</p>
+          </div>
+          <hr>
+          <div class="data">
+            <div>
+              <img src="path/to/images/${company.image}" alt="${company.name}" class="busiimg">
+            </div>
+            <div>
+              <p><strong>EMAIL:</strong> ${company.phone}</p>
+              <p><strong>PHONE:</strong> ${company.phone}</p>
+              <p><strong>URL:</strong> <a href="${company.website}" target="_blank">${company.website}</a></p>
+            </div>
+          </div>
+        `;
+        
+        membersContainer.appendChild(section);
+      });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+  
+  fetchMembers();
+  
