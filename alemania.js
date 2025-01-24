8141487b714d7287bba81db15e3aa0a32a6c3385
dmquiroga11 <qui24003@byui.edu>
2025-01-24 18:33:37 -0500

    img hero

---
+++ alemania.js
@@ -0,0 +1,32 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=5b320e33136604a278795d3bc7fc1682&units=imperial"; // Añadí &units=imperial para obtener la temperatura en Fahrenheit
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      displayResults(data); // llamada a la función para mostrar los datos
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
+
+
