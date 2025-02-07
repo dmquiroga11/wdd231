8374840c530dcd12245562aabbd41a506219ec78
dmquiroga11 <qui24003@byui.edu>
2025-02-06 20:00:54 -0500

    new images

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,22 @@
+/*--------------------------MENU-------------------------------*/
+const nav = document.querySelector("#nav");
+const open = document.querySelector("#openmenu");
+const close = document.querySelector("#closemenu");
+
+open.addEventListener("click", () => {
+    nav.classList.add("visible");
+});
+
+close.addEventListener("click", () => {
+    nav.classList.remove("visible");
+});
+/*--------------------LAST MODIFICATION-------------------------*/
+const lastModified = document.querySelector("#lastModified");
+lastModified.textContent = document.lastModified;
+
+const currentTime = document.querySelector("#currentTime");
+
+function updateTime() {
+    const now = new Date();
+    currentTime.textContent = now.toLocaleTimeString();
+}
