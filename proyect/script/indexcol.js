3afc59c611b58340ee575b607fd0fd5153672ff1
dmquiroga11 <qui24003@byui.edu>
2025-02-13 21:14:11 -0500

    images charge

--- proyect/script/indexcol.js
+++ proyect/script/indexcol.js
@@ -1,25 +1,25 @@
 /*-----------------IMG CHANGE-----------------*/
+document.addEventListener('DOMContentLoaded', (event) => {
+    const images = [
+        { src: 'https://raw.githubusercontent.com/dmquiroga11/wdd231/refs/heads/main/proyect/images/images1.webp', srcsetLarge: 'https://raw.githubusercontent.com/dmquiroga11/wdd231/refs/heads/main/proyect/images/images1.webp', alt: 'Imagen 1' },
+        { src: 'https://raw.githubusercontent.com/dmquiroga11/wdd231/refs/heads/main/proyect/images/images3.webp', srcsetLarge: 'https://raw.githubusercontent.com/dmquiroga11/wdd231/refs/heads/main/proyect/images/images3.webp', alt: 'Imagen 3' }
+    ];
 
-const images = [
-    { src: 'https://raw.githubusercontent.com/dmquiroga11/wdd231/refs/heads/main/proyect/images/images1.webp', srcsetLarge: 'https://raw.githubusercontent.com/dmquiroga11/wdd231/refs/heads/main/proyect/images/images1-large.webp', alt: 'Imagen 1' },
-    { src: 'https://raw.githubusercontent.com/dmquiroga11/wdd231/refs/heads/main/proyect/images/images3.webp', srcsetLarge: 'https://raw.githubusercontent.com/dmquiroga11/wdd231/refs/heads/main/proyect/images/images3-large.webp', alt: 'Imagen 3' }
-];
+    const imageChanger = document.getElementById('imageChanger');
+    let currentImageIndex = 0;
 
-const imageChanger = document.getElementById('imageChanger');
-let currentImageIndex = 0;
+    function changeImage() {
+        currentImageIndex = Math.floor(Math.random() * images.length);
+        imageChanger.querySelector('source[media="(min-width: 800px)"]').srcset = images[currentImageIndex].srcsetLarge;
+        imageChanger.querySelector('source[media="(min-width: 500px)"]').srcset = images[currentImageIndex].src;
+        imageChanger.querySelector('img').src = images[currentImageIndex].src;
+        imageChanger.querySelector('img').alt = images[currentImageIndex].alt;
+    }
 
-function changeImage() {
-    currentImageIndex = Math.floor(Math.random() * images.length);
-    imageChanger.querySelector('source[media="(min-width: 800px)"]').srcset = images[currentImageIndex].srcsetLarge;
-    imageChanger.querySelector('source[media="(min-width: 500px)"]').srcset = images[currentImageIndex].src;
-    imageChanger.querySelector('img').src = images[currentImageIndex].src;
-    imageChanger.querySelector('img').alt = images[currentImageIndex].alt;
-}
-
-changeImage();
+    changeImage();
+});
 
 
-changeImage();
 
 /*--------------------------MENU-------------------------------*/
 const nav = document.querySelector("#nav");
